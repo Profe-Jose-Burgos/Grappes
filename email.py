e17665454906feac58d31f444f720dcfb4cd9c6f
DasilverX <joseandresdasilva@outlook.com>
2023-01-23 17:06:25 -0500

    Ultimos cambios

--- email.py
+++ email.py
@@ -13,26 +13,27 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
 #Variables prueba
-name = 'Jose'
-quotation_number = 2003
+name = 'Jose Dasilva'
+quotation_number = rdm.randint(0,100)
 company = 'Apple Computers INC'
+date = datetime.date.today()
 
 #Creacion del Documento pdf
-doc = canvas.Canvas("documento_prueba.pdf",pagesize=A4)
+doc = canvas.Canvas("quotation.pdf",pagesize=A4)
 doc.setFont("Times-Roman", 12)
 doc.setLineWidth(.2)
 
 #Formando la plantilla
 doc.drawString(30,750,'Cotizacion %s'%quotation_number)
 doc.drawString(30,735,'Grappes INC')
-doc.drawString(500,755,'01/23/2023')
+doc.drawString(500,755,'%s'%date)
 doc.line(378,723,580,723)
 ##################################
 doc.drawString(275,725,'Estimado')
 doc.drawString(400,725,name)
 doc.line(378,723,580,723)
 
-doc.drawString(30,703,'Nombre')
+doc.drawString(30,703,'Empresa')
 doc.line(120,700,580,700)
 doc.drawString(120,703,'%s'%company)
 doc.showPage()
@@ -44,10 +45,10 @@ doc.save()
 time.sleep(1)
 sender = 'grappes.sic@hotmail.com'
 password = 'GrappesSic123'
-addressee = input('Ingresa destinatario: ')
-subject = input('Ingresa el asunto del correo: ')
-body_message = input('Ingresa el mensaje que deseas enviar: ')
-path_doc = input('Prueba documento (ingresa el nombre del archivo): ')
+addressee = 'grappes.sic@hotmail.com'
+subject = 'Cotizacion',quotation_number
+body_message = 'Hola %s, /n Gracias por solicitar una cotizacion con Grappes Shipping. Podras encontrar la cotizacion adjuntada en el correo.'
+path_doc = 'quotation.pdf'
 doc_name = 'Cotizacion'
 
 
@@ -57,7 +58,7 @@ menssage['From'] = sender
 menssage['To'] = addressee
 menssage['Subject'] = subject
 menssage.attach(MIMEText(body_message,'plain'))
-doc_attach = open(path_doc,'rb')
+doc_attach = open(path_doc)
 attach_MIMEbase = MIMEBase('application','octet-stream')
 attach_MIMEbase.set_payload((doc_attach).read())
 encoders.encode_base64(attach_MIMEbase)
